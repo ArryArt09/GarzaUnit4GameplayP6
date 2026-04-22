890e130f620b619e683f0680f731c1e39ad19cac
ArryArt09 <746785@student.dusd.net>
2026-04-22 13:45:16 -0700

    Do the GDD in freetime
    almost done, bum rush the rest of the project

--- P6GarzaUnit4Gameplay/Assets/Scripts/PlayerController.cs
+++ P6GarzaUnit4Gameplay/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     private GameObject focalPoint;
 
+    public bool hasPowerup;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,5 +24,22 @@ public class PlayerController : MonoBehaviour
 
         float sideInput = Input.GetAxis("Horizontal");
         playerRb.AddForce(focalPoint.transform.right * speed * sideInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Yo Walter, I think " + collision.gameObject.name + " hit the player while they were speaking " + hasPowerup + "fully.");
+        }
     }
 }
